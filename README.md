525c9846afc82bd4a0f4cd2c154364d547a6e2e5
Ryan Julyan <ryan@julyan.biz>
2025-02-12 21:11:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MSIT-5250-Written-Assignment-Unit-2
+
+Public Github Page Example for Unit 2
---
+++ script.js
@@ -0,0 +1,5 @@
+// Optional JavaScript functionality can go here.
+// For example, form validation, animations, or interactive elements.
+
+// This basic script will simply log a message to the browser console to prove it loaded.
+console.log("Welcome to TechNova Solutions!");
